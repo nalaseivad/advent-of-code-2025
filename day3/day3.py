3ac508dad87dcb232ce377dc222f9beaac0a7a11
Alan Davies <alan@alandavies.org>
2025-12-16 16:34:51 -0500

    Comments

--- day3/day3.py
+++ day3/day3.py
@@ -1,12 +1,27 @@
 import sys, re
 
 
+#
+# Take num_batteries sequential batteries from a list of batteries to form the maximum possible joltage
+#
+# E.g. batteries = [2 8 8 7 1 5 4], num_batteries = 3
+#
+# First we need to pick the max battery from the first 5 batteries.  We have to ignore the last two batteries since we
+# need to leave room for the other two batteries that we will pick next. So we pick 8, but which one?  We pick the first
+# one since that leaves us with the most choices for subsequent batteries. Once we've picked that first '8' battery then
+# we can't pick any batteries before it, so we remove all the batteries up to and including that first '8' battery. The
+# next set of candidate batteries is now [8 7 1 5 4]. We how have to ignore the last battery - since we need to leave
+# room to pick one more. So we pick the max battery from [8 7 1 5], which is 8. Again we pick the first one (there's
+# only the one in this case), and remove all batteries up to and including that battery. This leaves us with [7 1 5 4].
+# Now we just have to pick the max battery from the remaining batteries, which is 7. So the selected batteries are [8 8
+# 7], and the max joltage is 887.
+#
 def calc_max_joltage(batteries, num_batteries):
     digits = []
     for n in range(num_batteries - 1):
         digit = max(batteries[:n - (num_batteries - 1)])
         digits.append(digit)
-        batteries = batteries[batteries.index(digit) + 1:]
+        batteries = batteries[batteries.index(digit) + 1:]   # Remove up to and including the first occurrence of digit
     digit = max(batteries)
     digits.append(digit)
     max_joltage = int("".join(digits))
